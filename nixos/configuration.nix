a8c677f06155dbe8d69d7d48d85b2a34f6c9ca50
KillyBOT <edwards.kyle.j@gmail.com>
2025-06-26 23:31:42 -0400

    Added initial hashed passwd

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -54,8 +54,10 @@
       description = "frampt";
       isNormalUser = true;
 
+      initialHashedPassword = "$y$j9T$olffqlq8r/DdIwwQIfVZM.$1uct2hbKaSuoRQ2UEjyOGLqB5NPmLmnTZZzRw4O0p03";
       openssh.authorized.keys = [
         # Add public SSH keys here!
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlEVCyL79NGBJq9dkDXbwxQrUMnW5CpHezjOhYesYkC edwards.kyle.j@gmail.com"
       ];
 
       extraGroups = ["networkmanager" "wheel"];
