9c1d762842746fc028f49872892f3f590962390f
KillyBOT <edwards.kyle.j@gmail.com>
2025-11-15 22:53:37 -0500

    Fixed Librewolf settings
    Now my extensions should work as intended

--- users/frampt/librewolf.nix
+++ users/frampt/librewolf.nix
@@ -1,23 +1,17 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  # Use NUR here
-  nixpkgs.overlays = [
-    inputs.nur.overlays.default
-  ];
-
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
-    # Firefox but it doesn't spy on you!
+    # Firefox but it doesn't spy on you (probably)
     package = pkgs.librewolf;
-    profiles."user" = {
-      id = 0;
-      isDefault = true;
-
-      settings = {
-        "privacy.resistFingerprinting" = false;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudeies = true;
+      FirefoxHome = {
+        "Search" = false;
+      };
+      HardwareAcceleration = true;
+      Preferences = {
+        "privacy.resistFingerprinting" = false; # I'm sorry, it's just too annoying :(
 
         "privacy.fingerprintingProtection" = true;
         "privacy.fingerprintingProtection.overrides" = "+AllTargets,-CSSPrefersColorScheme";
@@ -26,11 +20,29 @@
         "privacy.clearOnShutdown.cookies" = false;
       };
 
-      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-        bitwarden
-        canvasblocker
-      ];
+      # Extensions (to get the IDs, go to about:support)
+      ExtensionSettings = {
+        # Bitwarden
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4599707/bitwarden_password_manager-2025.10.0.xpi";
+          installation_mode = "force_installed";
+        };
+        # CanvasBlocker
+        "CanvasBlocker@kkapsner.de" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4413485/canvasblocker-1.11.xpi";
+          installation_mode = "force_installed";
+        };
+        # SponsorBlock
+        "sponsorBlocker@ajay.app" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4608179/sponsorblock-6.1.0.xpi";
+          installation_mode = "force_installed";
+        };
+        # uBlock Origin (not technically needed, but just in case)
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4598854/ublock_origin-1.67.0.xpi";
+          installation_mode = "force_installed";
+        };
+      };
     };
   };
 }
